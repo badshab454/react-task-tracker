612ea479fa4cf9e8e027a8d58c31d55e8765799a
Badsha Hossain <badshab454@gmail.com>
2023-01-25 00:18:09 +0600

    Add task submit

--- src/App.js
+++ src/App.js
@@ -27,6 +27,13 @@ function App() {
     },
 ])
 
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+} 
+
 // Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
@@ -44,7 +51,7 @@ const toggleReminder = (id) => {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete = {deleteTask} onToggle={toggleReminder} /> ) : ( 'No Task To Show' )}
     </div>
   );
