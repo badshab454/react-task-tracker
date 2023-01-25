0209f4bb731f71582d741a371fd86f2456e4648a
Badsha Hossain <badshab454@gmail.com>
2023-01-25 23:39:10 +0600

    show add task state

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const toggleReminder = (id) => {
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete = {deleteTask} onToggle={toggleReminder} /> ) : ( 'No Task To Show' )}
     </div>
